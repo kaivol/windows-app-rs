828b1e791423f8347a86248e4099ab1894e4dc1b
kaivol <github@kavol.de>
2022-06-13 12:57:11 +0200

    simplify manifest creation

--- crates/libs/windows-app-deploy/src/lib.rs
+++ crates/libs/windows-app-deploy/src/lib.rs
@@ -1,27 +1,42 @@
 #[rustfmt::skip]
 mod generated;
 
-use embed_manifest::{embed_manifest, new_manifest};
 use std::env;
-use std::fs::OpenOptions;
+use std::fs::File;
 use std::io::Write;
+use std::path::PathBuf;
 
 /// Writes the Windows App Runtime Bootstrap dll to `OUT_DIR` and embeds a manifest into the executable.
 /// This is only useful when called from a build script.
-pub fn to_output_dir() {
-    let mut path =
-        std::path::PathBuf::from(std::env::var("OUT_DIR").expect("No `OUT_DIR` env variable set"));
+pub fn dll_to_output_dir() {
+    let mut path = PathBuf::from(env::var("OUT_DIR").expect("No `OUT_DIR` env variable set"));
     path.pop();
     path.pop();
     path.pop();
     path.push("Microsoft.WindowsAppRuntime.Bootstrap.dll");
-    if let Ok(mut file) = OpenOptions::new().create_new(true).write(true).open(&path) {
-        file.write_all(&generated::BOOTSTRAP_DLL_BYTES).unwrap();
-    }
-
-    embed_manifest(new_manifest(
-        &env::var("CARGO_PKG_NAME").expect("Unable to read package name"),
-    ))
-    .expect("unable to embed manifest file");
+    File::create(&path)
+        .unwrap()
+        .write_all(&generated::BOOTSTRAP_DLL_BYTES)
+        .unwrap();
     println!("cargo:rerun-if-changed={}", path.display());
+}
+
+pub fn embed_manifest() {
+    let out_dir = env::var("OUT_DIR").expect("No `OUT_DIR` env variable set");
+    let manifest_file = PathBuf::from(out_dir).join("manifest.xml");
+    File::create(&manifest_file)
+        .unwrap()
+        .write_all(
+            r#"<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0"></assembly>
+"#
+            .as_bytes(),
+        )
+        .unwrap();
+    println!("cargo:rustc-link-arg-bins=/MANIFEST:EMBED");
+    println!(
+        "cargo:rustc-link-arg-bins=/MANIFESTINPUT:{}",
+        manifest_file.display()
+    );
+    println!("cargo:rustc-link-arg-bins=/MANIFESTUAC:NO");
 }
--- crates/samples/mrm/build.rs
+++ crates/samples/mrm/build.rs
@@ -1,3 +1,4 @@
 fn main() {
-    windows_app_deploy::to_output_dir();
+    windows_app_deploy::dll_to_output_dir();
+    windows_app_deploy::embed_manifest();
 }
